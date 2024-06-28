386a6be16da34a946eeb5e1738947475a586ef13
Nikola <nikola.malic95@gmail.com>
2024-06-28 15:28:13 +0200

    change oracle indexer cronjob rate from 1 min to 30 mins

--- src/oracle-indexer.ts
+++ src/oracle-indexer.ts
@@ -8,18 +8,27 @@ async function reconcile(chainid: number) {
     console.log(`[${chainid}] Reconciling db with subgraph`);
 
     const config = {
-        method: 'post',
+        method: "post",
         url: SUBGRAPH_URLS[chainid],
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
-        data: oracleQuery
+        data: oracleQuery,
     };
-    const res = await axios(config) as OracleQueryResponse
+    const res = (await axios(config)) as OracleQueryResponse;
     const graph_oracles = [...new Set(res.data.data.setTokenOracles)];
     const db_oracles = await readOracles(chainid);
-    const distinct_graph_oracles = [...new Set(graph_oracles.map( oracle => oracle.token ))]
-    console.log(`[${chainid}]`, "Found", graph_oracles.length, "oracles in subgraph", distinct_graph_oracles.length, "distinct, Found", db_oracles.length, "oracles in db");
+    const distinct_graph_oracles = [...new Set(graph_oracles.map(oracle => oracle.token))];
+    console.log(
+        `[${chainid}]`,
+        "Found",
+        graph_oracles.length,
+        "oracles in subgraph",
+        distinct_graph_oracles.length,
+        "distinct, Found",
+        db_oracles.length,
+        "oracles in db",
+    );
     for (const oracle of graph_oracles) {
         if (!db_oracles.includes(oracle.token)) {
             await addOracle({
@@ -35,12 +44,13 @@ async function main() {
     await connect();
     console.log("Connected to db");
 
-    cron.schedule("*/1 * * * *", async () => {
-        await Promise.all(CHAIN_IDS.map((chainid) => {
-            return reconcile(chainid);
-        }))
+    cron.schedule("*/30 * * * *", async () => {
+        await Promise.all(
+            CHAIN_IDS.map(chainid => {
+                return reconcile(chainid);
+            }),
+        );
     });
 }
 
-
-main().then()
+main().then();
